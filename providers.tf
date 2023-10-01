ed273e124ae9ebe27a94707e62db99632f4658da
Rajaaa2017 <95734759+Rajaaa2017@users.noreply.github.com>
2023-10-02 03:04:31 +0530

    #24 removed random provider and added doc to the configuration drift (#25)

--- providers.tf
+++ providers.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = "3.5.1"
+    # }
     aws = {
       source  = "hashicorp/aws"
       version = "5.16.2"
     }
   }
-  cloud {
-    organization = "bootcamp-demox"
+  # cloud {
+  #   organization = "bootcamp-demox"
 
-    workspaces {
-       name = "Terra-house-1"
-    }
-  }
+  #   workspaces {
+  #      name = "Terra-house-1"
+  #   }
+  # }
 }
 
 provider "aws" {
