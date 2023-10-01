00c626799459a9cffc1fa2b0fc155570cba7885c
Rajaaa2017 <95734759+Rajaaa2017@users.noreply.github.com>
2023-10-02 01:38:07 +0530

    #22 Restructure Root Module (#23)

---
+++ providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+  cloud {
+    organization = "bootcamp-demox"
+
+    workspaces {
+       name = "Terra-house-1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+provider "random" {
+  # Configuration options
+}
