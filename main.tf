9340ef75ba7bbbee08e58e7e6b18555248570bf3
Rajaaa2017 E <rajashekar.e2017@gmail.com>
2023-10-09 17:55:13 +0000

    #42 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 terraform {
-
+    required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
 }
 
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+/*
 module "terrahouse_aws" {
   source              = "./modules/terrahouse_aws"
   user_uuid           = var.user_uuid
@@ -11,3 +23,4 @@ module "terrahouse_aws" {
   assets_path         = var.assets_path
   content_version     = var.content_version
 }
+*/
